880017ca7c16c96877d2a26282d5ff41bc2daa5e
hamilton moore <hamlmoor@gmail.com>
2017-08-20 16:05:28 -0500

    first draft of html

--- server.js
+++ server.js
@@ -4,23 +4,30 @@ const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const path = require("path");
+const sessionConfig = require("./sessionConfig");
+const expressValidator = require("express-validator");
 const port = process.env.PORT || 8000;
+// const fs = require('file-system');
+// const words = fs
+//     .readFileSync("/usr/share/dict/words", "utf-8")
+//     .toLowerCase()
+//     .split("\n");
 
-// const sessionConfig = require("./sessionConfig");
 // const users = require("./data");
-// const checkAuth = require("./middlewares/checkAuth");
 
 const app = express();
 
 //TEMPLATE ENGINES
 app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
+app.use(expressValidator());
 app.set("view engine", "mustache");
 
 //MIDDLEWARES
 app.use(express.static(path.join(__dirname, "./public")));  //access to css
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session(sessionConfig));
 
 
 app.get("/", (req, res) => {   //when root("/") is entered into the browser, it requests a response (????)
---
+++ sessionConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+    secret: "bubble pickle street converse",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+};
