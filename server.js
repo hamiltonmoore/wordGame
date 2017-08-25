3f19af3b78a8ba2537d3fc2c9662babdf980d013
hamilton moore <hamlmoor@gmail.com>
2017-08-25 16:56:31 -0500

    refactored functions

--- server.js
+++ server.js
@@ -31,51 +31,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionConfig));
 
 //word generator
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
 
 
 //loop through word and push __ per length 
 
 app.get("/", function (req, res) {
-    res.render("home", req.session);
-})
-
-//this corresponds to the button to start game/resets arrays(?)
-app.post("/startGame", function (req, res) {
-    req.session.word = words[getRandomInt(0, words.length - 1)];
-    req.session.displayArray = [];
-    req.session.wrongGuesses = [];
-    req.session.correctGuesses = [];
-    req.session.turns = 8;
-    for (let i = 0; i < req.session.word.length; i++) {
-        req.session.displayArray.push("__");
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
-    res.redirect("/");
+    let game = {};
+    game.word = words[getRandomInt(0, words.length - 1)];
+    game.displayArray = [];
+    game.wrongGuesses = [];
+    game.correctGuesses = [];
+    game.turns = 8;
+    for (let i = 0; i < game.word.length; i++) {
+        game.displayArray.push("__");
+    }
+    console.log("turns = ", game.turns);
+    req.session.game = game;
+    return res.render("home", game); //don't pass the session
 });
-
 //this stores the guess and compares it to the random word
-app.post("/guessletter", (req, res) => {
-    let guessLetter = req.body.letterGuess
-    if (req.session.wrongGuesses.indexOf(guessLetter) > -1 ||
-        req.session.correctGuesses.indexOf(guessLetter)) {
-        req.session.msg = "some message";
+app.post("/home", (req, res) => {
+    let game = req.session.game; //game is assigned FROM session
+    let guessLetter = req.body.letterGuess // this is where the letter is 
+    if (alreadyGuessed(game, guessLetter)) {
+        saveGame(req, game, "Already guessed");
+        console.log(guessletter);
         return res.redirect("/");
     }
-    for (i = 0; i < req.session.word.length; i++) {
-        if (word.charAt(i) === guessLetter) {
-            display[i] = guessLetter;
+    for (i = 0; i < game.word.length; i++) {
+        if (game.word.charAt(i) === guessLetter) {
+            game.displayArray[i] = guessLetter;  // TODO: 
         }
     }
-    if (word.indexOf(guessLetter) < 0) {
-        req.session.msg = "WRONG";
-        req.session.wrongGuesses.push(guessletter);
-        req.session.turns -= 1 //decrement turns
+    if (letterNotFound(game, guessLetter)) {
+        game.wrongGuesses.push(guessletter);
+        game.turns -= 1 //decrement turns
+        saveGame(req, game, "WRONG"); //setting session game = to local game/game is put into session
         return res.redirect("/");
     }
-    if (req.session.turns < 1) {
-        req.session.msg = "no more turns, game over!"
+    if (game.turns < 1) {
+        saveGame(req, game, "no more turns, game over!");
         return res.redirect("/");
     }
 });
@@ -104,3 +102,17 @@ app.post("/guessletter", (req, res) => {
 app.listen(port, () => {
     console.log(`you are on port ${port}`);
 });
+
+function saveGame(req, game, msg) {
+    game.msg = msg;
+    req.session.game = game;
+}
+
+function letterNotFound(game, guessLetter) {
+    return game.word.indexOf(guessLetter) < 0;
+}
+
+function alreadyGuessed(game, guessLetter) {
+    return game.wrongGuesses.indexOf(guessLetter) > -1 ||
+        game.correctGuesses.indexOf(guessLetter)
+}
