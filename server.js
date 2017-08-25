9e98d2b02fcf15c4541d010ffaf060cdaba53f91
hamilton moore <hamlmoor@gmail.com>
2017-08-25 10:48:00 -0500

    placed loop inside of post request

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const session = require("express-session");
 const path = require("path");
 const sessionConfig = require("./sessionConfig");
 const expressValidator = require("express-validator");
-
 const port = process.env.PORT || 8000;
 //get the dictionary 
 const fs = require('fs');
@@ -34,25 +33,30 @@ app.use(session(sessionConfig));
 //word generator
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-    req.session.word = randomWord;
 }
-const randomWord = words[getRandomInt(0, 235887)];
-let displayArray = [];
-let guessArray = [];
-//     displayArray.push(randomWord.length);
-// console.log(displayArray);
-for (let i = 0; i < randomWord.length; i++) {
-    guessArray.push("__");
-}
-app.get("gameForm", function (req, res) {
-    req.session.display = displayArray;
+
+
+//loop through word and push __ per length 
+
+app.get("/", function (req, res) {
+    res.render("home", req.session);
+})
+
+//this corresponds to the button to start game/resets arrays(?)
+app.post("/startGame", function (req, res) {
+    req.session.word = words[getRandomInt(0, words.length - 1)];
+    req.session.displayArray = [];
     req.session.wrongGuesses = [];
     req.session.correctGuesses = [];
     req.session.turns = 8;
+    for (let i = 0; i < req.session.word.length; i++) {
+        req.session.displayArray.push("__");
+    }
     res.redirect("/");
 });
 
-app.post("/", (req, res) => {
+//this stores the guess and compares it to the random word
+app.post("/guessletter", (req, res) => {
     let guessLetter = req.body.letterGuess
     if (req.session.wrongGuesses.indexOf(guessLetter) > -1 ||
         req.session.correctGuesses.indexOf(guessLetter)) {
@@ -78,24 +82,24 @@ app.post("/", (req, res) => {
 //don't keyboard programing (trying to write out code as you think of conditions)
 //instead comment out requirements as you go, and code each requirement
 
-app.post("/home", (req, res) => {
-    let letterGuess = req.body.letterGuess; //this gets the letter that was guessed
-    let locationOfLetter = randomWord.indexOf(letterGuess) //location of letter
-    if (randomWord.includes(letterGuess) == true) {
-        console.log("it's there, good job");
-        guessArray.splice(locationOfLetter, 1, letterGuess);
-    }
-    else {
-        res.send("NOPE, try again")
-    }
-});
+// app.post("/home", (req, res) => {
+//     let letterGuess = req.body.letterGuess; //this gets the letter that was guessed
+//     let locationOfLetter = randomWord.indexOf(letterGuess) //location of letter
+//     if (randomWord.includes(letterGuess) == true) {
+//         console.log("it's there, good job");
+//         guessArray.splice(locationOfLetter, 1, letterGuess);
+//     }
+//     else {
+//         res.send("NOPE, try again")
+//     }
+// });
 
-app.get("/", (req, res) => {   //when root("/") is entered into the browser, it requests a response (????)
-    res.render("home", {
-        randomWord: randomWord,
-        guessArray: guessArray,
-    });
-})
+// app.get("/", (req, res) => {   //when root("/") is entered into the browser, it requests a response (????)
+//     res.render("home", {
+//         randomWord: randomWord,
+//         guessArray: guessArray,
+//     });
+// })
 
 app.listen(port, () => {
     console.log(`you are on port ${port}`);
